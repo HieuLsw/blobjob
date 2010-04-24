e7b42131acd3e67d78fb235cb8a11b5f61229a29
Lucas Caro <lucascaro@gmail.com>
2010-04-24 15:10:48 -0300

    added lives OSD and corrected live decrement

--- gamelib/game_map.py
+++ gamelib/game_map.py
@@ -1,5 +1,6 @@
 import os
 
+from cocos.text import Label
 from cocos.actions import Repeat
 from cocos.actions.grid3d_actions import *
 from cocos.actions.camera_actions import *
@@ -17,8 +18,45 @@ import data
 import sounds
 import pyglet
 import game_scene
-lives = 1
 
+#Character extra lives
+#shoudl be an attribute of the character class
+lives = 2
+
+
+class OSDLayer(Layer):
+    def __init__(self):
+        super(OSDLayer,self).__init__()
+
+        #blob image
+        self.lives_sprite = Sprite("chars/wobble/blob_1.png")
+        self.add(self.lives_sprite);
+        self.lives_sprite.x = 720
+        self.lives_sprite.y = 575
+        self.lives_sprite.scale = 0.4
+        self.lives_sprite.opacity = 100
+
+        #lives text
+        font_name = "Casual"
+        font_size = 10
+        fo = pyglet.font.load(font_name, font_size)
+        font_item = {}
+        font_item['font_name'] = font_name
+        font_item['font_size'] = font_size
+        font_item['anchor_x'] = "right"
+        font_item['color'] = (130,185,85,200)
+        font_item['x'] = 770
+        font_item['y'] = 550
+        
+        self.label = Label(**font_item )
+
+        self.update()
+        self.add(self.label)
+        
+
+    def update(self):
+        global lives
+        self.label.element.text = 'x '+str(lives)
 
 class GameDecoratorLayer(Layer):
     def __init__(self):
@@ -58,7 +96,10 @@ class GameMapScene(GameMapBaseScene):
         self.wobble.y=0
         self.manager.y = -25
         self.fg = GameDecoratorLayer()
+        self.osd = OSDLayer()
+        
         self.add(self.fg,z=999)
+        self.add(self.osd,z=1000)
         sounds.set_music('music/on_game.ogg')
         #This line fixes the grid added by the MapLayer
         self.manager.do(Waves(waves=1,amplitude=0))
@@ -67,7 +108,8 @@ class GameMapScene(GameMapBaseScene):
         global lives
         if(lives>0):
             director.replace(FadeTransition(game_scene.cur_level(),duration=1))
-            lives -= lives
+            lives -= 1
+            self.osd.update()
         else:
             director.replace(FadeTransition(game_scene.GameOverScene(),duration=1))
             lives = 1
